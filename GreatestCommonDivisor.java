22a89e8b9e800b1efd047062ba16327bcd85b113
syedhamzasiff <113232083+syedhamzasiff@users.noreply.github.com>
2022-11-18 10:23:18 +0500

    Add files via upload

---
+++ GreatestCommonDivisor.java
@@ -0,0 +1,15 @@
+
+package greatestcommondivisor;
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        
+    }
+    public static int gcd(int a, int b){
+        if(a==0 || b==0)
+            
+        }
+                   
+                    
+    }
+    
+}
