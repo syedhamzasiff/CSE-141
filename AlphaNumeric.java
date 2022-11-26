2aef6e992a61ce83bc96e949233428fb663d6751
syedhamzasiff <113232083+syedhamzasiff@users.noreply.github.com>
2022-11-26 09:26:41 +0500

    Add files via upload

---
+++ AlphaNumeric.java
@@ -0,0 +1,10 @@
+
+package alphanumeric;
+import java.util.*;
+public class AlphaNumeric {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+    }
+    
+}
